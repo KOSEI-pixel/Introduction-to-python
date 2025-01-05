6aa349c738a1901c4041f2f916426fb5179ca14d
kosei-pixel <kosie949@gmail.com>
2025-01-05 16:53:56 +0000

    Add interactive user input and additional print statements to main.py and create main2.py for user registration

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+#Commenting in Python
 print("hi my name is main.py and i will be using the module mymodule.py to code some stuff with my teacher\n"
       "i will be coding with my classmate named Dawala")
+
+print("today is my second day of coding with my teacher and my classmate USING PYTHON LANGUAGE, and im hoping it\n"
+      "will be a great day and fun")
+print(69)
+print("i am ,12, years old")
+print("i was born on june 14 2012")
+      
---
+++ main2.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+number = int(input("Enter your  registeration_number: "))
+age = int(input("Enter your Age: "))
+grade = int(input("Enter your grade/year: "))
+print("\n hello", name, "welcome to our services, \n your number is", number, "and your age is", age,
+      "years old, and your grade/year is", grade, "and thank you for using our services")
